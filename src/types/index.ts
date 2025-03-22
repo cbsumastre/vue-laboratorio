500720cecaccb30131c320569f0cd721f476d847
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-03-22 18:49:41 +0100

    added barrel types

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+import type { Task } from "@/types/task"
+import { TaskStatus } from "@/types/task"
+import type { Filter } from "@/types/filter"
+
+export type {
+  Task,
+  Filter
+}
+
+export default TaskStatus
