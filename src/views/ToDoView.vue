5e17a69bd2a9dfa66c6909aa8efa9381341066e4
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-03-23 18:42:32 +0100

    refact main component

--- src/views/ToDoView.vue
+++ src/views/ToDoView.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import ToDoFilters from '@/components/ToDoFilters.vue'
+import ToDoSearch from '@/components/ToDoSearch.vue'
+import ToDoForm from '@/components/ToDoForm.vue'
+import ToDoTaskList from '@/components/ToDoTaskList.vue'
 </script>
 
 <template>
@@ -6,5 +10,9 @@
     <header>
       <h1>ToDo App</h1>
     </header>
+    <ToDoForm />
+    <ToDoSearch />
+    <ToDoFilters />
+    <ToDoTaskList />
   </div>
 </template>
